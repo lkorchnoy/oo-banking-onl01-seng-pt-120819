9cf6b86adf1f725f407ca3c77333594bda093418
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-24 03:03:10 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,27 @@
 class BankAccount
+   @@balance = 1000
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @@balance = 1000
+    @status = open
+  end
+  
+  def deposit(n)
+    @@balance << deposit(n)
+end
+
+def display_balance
+  @@balance
+end
+
+def valid(balance)
+  balance.each {|b| b > 0, @status = open}
+end
+
+def close_account(balance)
+  balance.each {|b| if b < 0, @status != open}
+end
 
 end
